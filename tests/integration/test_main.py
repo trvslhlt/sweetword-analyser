f60801ac59b3f1c6f7c5633d2f195c4fc1b3dbb9
trvslhlt <trvslhlt@gmail.com>
2016-11-14 15:11:31 -0500

    Test writes sweetwords lists to csv

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,13 +1,39 @@
 import sys
+import tempfile
+import random
+import os
+import subprocess
+import codecs
+import csv
 
 # GOALS:
 # validate program interface
 # - input: python your_program.py m n filename
 # - output: ???
 
+sweetwordLists = [
+    ['notThePassword', 'maybeThePassword', 'definitelyThePassword'],
+    ['Monday', 'Tuseday', 'Wednesday'],
+    ['colorless', 'green', 'ideas']
+]
+
+sweewordListCount = len(sweetwordLists)
+sweetwordsInList = len(sweetwordLists[0])
 
 def main():
-    print('main')
+    #create a temp directory
+    tempDirectoryPath = tempfile.gettempdir() + '/' + str(random.randint(0,9999999))
+    print('tempDirectoryPath:', tempDirectoryPath)
+    os.mkdir(tempDirectoryPath)
+
+    # create sample sweetwords list file
+    sweetwordsListsFilepath = tempDirectoryPath + '/sweetwords.csv'
+    with open(sweetwordsListsFilepath, 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerows(sweetwordLists)
+
+
+
 
 
 if __name__ == '__main__':
