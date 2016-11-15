e5bfc75fffe92405569ef972512a2db365da9c04
trvslhlt <trvslhlt@gmail.com>
2016-11-14 20:20:11 -0500

    Add sample honeywords

--- src/sweetword_analyser.py
+++ src/sweetword_analyser.py
@@ -1,5 +1,29 @@
 import sys
+import csv
+
+
+def main(sweetwordListCount, sweetwordsPerList, sweetwordsListsFilepath):
+    sweetwordLists = readSweetwords(sweetwordsListsFilepath)
+    print(sweetwordLists)
+    # passwordIndices = getPasswordIndicesForSweetwordLists(sweetwordsLists)
+    # print(passwordIndices)
+
+
+def readSweetwords(sweetwordsListsFilepath):
+    sweetwordsLists = []
+    with open(sweetwordsListsFilepath, 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            sweetwords = row.split(',')
+            sweetwordLists.append(sweetwords)
+    return sweetwordsLists
+
+
+def getPasswordIndicesForSweetwordLists(sweetwordLists):
+    return passwordIndices
+
 
 if __name__ == '__main__':
     assert len(sys.argv) is 4, 'Args: number of lists, sweetwords per list, sweetword list filepath'
-    print('1,2,0')
+    [_, sweetwordListCount, sweetwordsPerList, sweetwordsListsFilepath] = sys.argv
+    main(sweetwordListCount, sweetwordsPerList, sweetwordsListsFilepath)
