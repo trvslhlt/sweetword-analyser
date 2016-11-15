8a297cd22173eaeccbf86a25baeeb616191f9a30
trvslhlt <trvslhlt@gmail.com>
2016-11-14 20:39:15 -0500

    Validating script arguments against csv data

--- src/sweetword_analyser.py
+++ src/sweetword_analyser.py
@@ -3,25 +3,33 @@ import csv
 
 
 def main(sweetwordListCount, sweetwordsPerList, sweetwordsListsFilepath):
-    sweetwordLists = readSweetwords(sweetwordsListsFilepath)
-    # passwordIndices = getPasswordIndicesForSweetwordLists(sweetwordsLists)
-    # print(passwordIndices)
+    sweetwordLists = readSweetwords(sweetwordsListsFilepath, sweetwordListCount, sweetwordsPerList)
+    passwordIndices = getPasswordIndicesForSweetwordLists(sweetwordLists)
+    print(passwordIndices)
 
 
-def readSweetwords(sweetwordsListsFilepath):
+def readSweetwords(sweetwordsListsFilepath, expectedListCount, expectedSweetwordsPerList):
+    print(expectedListCount, expectedSweetwordsPerList)
     sweetwordLists = []
     with open(sweetwordsListsFilepath, 'rb') as f:
         reader = csv.reader(f)
         for row in reader:
+            print len(row), expectedSweetwordsPerList
+            assert len(row) is expectedSweetwordsPerList, 'The number of sweetwords in the list was unexpected'
             sweetwordLists.append(row)
+    print len(sweetwordLists)
+    assert len(sweetwordLists) is expectedListCount, 'The number of sweetword lists was unexpected'
     return sweetwordLists
 
 
 def getPasswordIndicesForSweetwordLists(sweetwordLists):
+    passwordIndices = []
+    for sweetwordList in sweetwordLists:
+        passwordIndices.append(0)
     return passwordIndices
 
 
 if __name__ == '__main__':
     assert len(sys.argv) is 4, 'Args: number of lists, sweetwords per list, sweetword list filepath'
     [_, sweetwordListCount, sweetwordsPerList, sweetwordsListsFilepath] = sys.argv
-    main(sweetwordListCount, sweetwordsPerList, sweetwordsListsFilepath)
+    main(int(sweetwordListCount), int(sweetwordsPerList), sweetwordsListsFilepath)
